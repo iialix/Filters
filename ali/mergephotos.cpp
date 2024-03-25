f3a350148068c86491e456cc7bd250c730e4719c
iialix <alimadred552@gmail.com>
2024-03-25 16:26:46 +0200

    addedtwo filters

---
+++ ali/mergephotos.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+#include "Image_Class.h"
+
+int main()
+{
+    string image1;
+    cout << "Pls enter the name of the first image: ";
+    cin >> image1;
+
+    string image2;
+    cout << "Pls enter the name of the second image: ";
+    cin >> image2;
+
+    Image fimage(image1);
+    Image simage(image2);
+    Image newimage(simage.width, simage.height); 
+
+    for (int i = 0; i < fimage.width; ++i)
+    {
+        for (int j = 0; j < fimage.height; ++j)
+        {
+            unsigned int blue_avg, green_avg, red_avg;
+            blue_avg = green_avg = red_avg = 0;
+            for (int k = 0; k < 3; ++k)
+            {
+                
+            }
+            blue_avg = fimage(i, j, 2) + simage(i, j, 2);
+            green_avg = fimage(i, j, 1) + simage(i, j, 1);
+            red_avg = fimage(i, j, 0) + simage(i, j, 0);
+
+            newimage(i, j, 0) = red_avg / 2;
+            newimage(i, j, 1) = green_avg / 2;
+            newimage(i, j, 2) = blue_avg / 2;
+        }
+    }
+    string imagename;
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+    cin >> imagename;
+
+    newimage.saveImage(imagename);
+    system(imagename.c_str());
+
+
+
+
+    return 0;
+}
+ 
--- ali/start.cpp
+++ ali/start.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
-int main()
+unsigned long long factorial(int n) 
 {
+    int x = 1;
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        for(int i = n; i > 1; i--)
+        {
+            x = x * i;
+        }
+    }
+    return x;
+}
+long long gcd(int a, int b) 
+{
+    while (b != 0) {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() 
+{
+    long long a,b;
+    cin >> a >> b;
+
+    long long result1, result2;
+    result1 = factorial(a);
+    result2 = factorial(b);
+    int GCD;
+    GCD = gcd(result1, result2);
+    cout << GCD;
 
 }
